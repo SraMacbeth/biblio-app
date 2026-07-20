df6858a0af848b70152b897ffe087f842f25c86c
Emilia Poletti <emiliapol81@gmail.com>
2026-07-20 15:59:30 -0300

    feat(members): crear estructura inicial de MembersView

--- app/views/members_view.py
+++ app/views/members_view.py
@@ -1,6 +1,6 @@
 from tkinter import *
 from views.base_view import BaseView
-
+from components.search_bar_frame import SearchBar
 
 class MembersView(BaseView):
 
@@ -9,7 +9,18 @@ class MembersView(BaseView):
         self.controller = controller
         self.user = user
 
-        self.label = Label(self.main_area, text="Members View")
-        self.label.grid(row=0, column=0, padx=10, pady=20, sticky="nsew")
+        self.search_bar = SearchBar(
+            self.main_area,
+            search_callback=None,
+            search_type='member',
+            show_entity_selection=False)
+        self.search_bar.grid(row=0, column=0, sticky="ew")
+        self.search_bar.set_text_label(
+            "Ingrese el DNI del socio que desea buscar:")
 
-        self.grid_rowconfigure(1, weight=1)
+        self.new_member_button = Button(
+            self.main_area,
+            text="Agregar nuevo socio")
+        self.new_member_button.grid(row=3, column=0, columnspan=4, pady=10)
+
+        self.main_area.grid_columnconfigure(0, weight=1)
